7f773d0b6e4a46c13dc8f16607c16e6a6653f2f4
Tim A. Conner <timaconner1@gmail.com>
2018-01-19 14:21:48 -0600

    Add functions signature.

---
+++ assets/js/firebaseModel.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports.getWishlist = (uid) => {
+
+};
+
+module.exports.getWatchedlist = (uid) => {
+    // where dateWatched !== 0
+
+};
+
+module.exports.rateMovie = (uid, id, stars) => {
+    
+};
+
+module.exports.addMovie = (uid, id, date) => {
+    
+};
+
+module.exports.watchMovie = (uid, id, date) => {
+    
+};
+
+module.exports.deleteMovie = (uid, id) => {
+    
+};
+
+module.exports.searchWishlist = (uid, term) => {
+    
+};
+
+module.exports.searchwatched = (uid, term) => {
+    
+};
