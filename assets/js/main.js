7e18ee2da812ca0b96f15a826397d9f9bbb02019
Hunter Phillips <hkphillips42@Hunters-MacBook-Pro.local>
2018-01-22 09:24:45 -0600

    userAuth,search/show movies,getCast,poster

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
 "use strict";
+const $ = require('jquery'); 
+const controller = require('./controller');
+
+
+controller.activateListeners();
--- assets/js/view.js
+++ assets/js/view.js
@@ -1,11 +1,26 @@
 'use strict'; 
 const $ = require('jquery'); 
 const tmdb = require('./tmdb');
-const userData = require('./firebase');
+const userData = require('./firebaseModel');
+const auth = require("./user-factory");
 
 
 module.exports.showMovies = (moviesArr)=>{
    // clearMovies()
+   moviesArr.forEach(movie => {
+      let actors;
+      tmdb.getCast(movie.id)
+         .then(cast=>{
+            actors = tmdb.makeCastList(cast).join(', ');
+            $('#movieOutput').append(`
+               <div class='movieCards'>
+               <h4 id="${movie.id}">${movie.title}</h4>
+               <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="">
+               <p>${actors}</p>
+               <div>
+            `);
+         });
+   });
 };
 
 module.exports.activateTab = (tab)=>{
