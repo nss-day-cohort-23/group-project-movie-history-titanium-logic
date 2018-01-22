c7e7ff5742b050141286fcd44b0bb863d4b8c820
Tim A. Conner <timaconner1@gmail.com>
2018-01-22 12:12:15 -0600

    Get year of release date.

--- assets/js/view.js
+++ assets/js/view.js
@@ -8,12 +8,16 @@ const movieCard = require("./../templates/movieCard.hbs");
 module.exports.showMovies = (moviesArr)=>{
    // clearMovies()
    $('#movieList .row').html('');
-   
+  
+  moviesArr.map(movie => {
+    movie.release_date = movie.release_date.substring(0, 4);
+    return movie;
+  });
    moviesArr.forEach((movie, key) => {
      if (key < 6) {
       let actors;
       tmdb.getCast(movie.id)
-         .then(cast=>{
+         .then(cast => {
             actors = tmdb.makeCastList(cast).join(', ');
             $('#movieList .row').append(movieCard({movie, actors}));
          });
