33970fb0e88d73a8a6d7768c165f21e4c25cf2a3
Tim A. Conner <timaconner1@gmail.com>
2018-01-22 13:26:40 -0600

    Add class to movie.

--- assets/js/view.js
+++ assets/js/view.js
@@ -6,11 +6,20 @@ const auth = require("./user-factory");
 const movieCard = require("./../templates/movieCard.hbs");
 
 module.exports.showMovies = (moviesArr)=>{
-   // clearMovies()
-   $('#movieList > .row').html('');
+  // clearMovies()
+  $('#movieList > .row').html('');
   
   moviesArr.map(movie => {
     movie.release_date = movie.release_date.substring(0, 4);
+    if(typeof movie.stars !== "undefined"){
+      if(movie.stars >= 8){
+        movie.class = "favorite";
+      } else if(movie.stars === 0){
+        movie.class = "wishlist";
+      } else {
+        movie.class = "watched";
+      }
+    }
     return movie;
   });
 
