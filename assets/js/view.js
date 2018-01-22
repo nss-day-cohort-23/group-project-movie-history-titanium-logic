bf3bcdc101c9b581a85c79a0b8506c649feff774
Tim A. Conner <timaconner1@gmail.com>
2018-01-22 14:04:42 -0600

    Tac favorites (#27)
    Generate stars in handelbars.
    
    * Add stars generated in handelbars.
    
    * Generate stars in handelbars.

--- assets/js/view.js
+++ assets/js/view.js
@@ -5,22 +5,39 @@ const userData = require('./firebaseModel');
 const auth = require("./user-factory");
 const movieCard = require("./../templates/movieCard.hbs");
 
-module.exports.showMovies = (moviesArr) => {
-    clearMovies();
 
-    moviesArr.map(movie => {
-        movie.release_date = movie.release_date.substring(0, 4);
-        if (typeof movie.stars !== "undefined") {
-            if (movie.stars >= 8) {
-                movie.class = "favorite";
-            } else if (movie.stars === 0) {
-                movie.class = "wishlist";
-            } else {
-                movie.class = "watched";
-            }
+module.exports.showMovies = (moviesArr)=>{
+  clearMovies();
+
+  moviesArr.map(movie => {
+    movie.release_date = movie.release_date.substring(0, 4);
+    
+    if(typeof movie.stars !== "undefined"){
+      if(movie.stars >= 8){
+        movie.class = "favorite";
+      } else if(movie.stars === 0){
+        movie.class = "wishlist";
+      } else {
+        movie.class = "watched";
+      }
+
+      // Add  objects for handelbars to loop over to know the count.  Handlebars does not have a for loop, but it does have foreach.
+
+      let movieStars = [];
+      for(let i = 0; i < 10; i++){ 
+        if(i < movie.stars){
+          movieStars.push({star: true});
+        } else {
+          movieStars.push({});
         }
-        return movie;
-    });
+      }
+      movie.stars = movieStars;
+
+    }
+    
+
+    return movie;
+  });
 
     moviesArr.forEach((movie, key) => {
         if (key < 6) {
