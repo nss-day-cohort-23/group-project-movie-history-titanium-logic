5ff680fc92625e40d63b4c854affa57e6da6fb2d
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-01-23 12:05:19 -0600

    Kb integrated search (#40)
    * search returns firebase results at the top, need to grab tmdb info for fb movies
    
    * tmdb populates data from firebase calls, but out of order
    
    * indented view, made showMovies go in order of search, not promise speed

--- assets/js/firebaseModel.js
+++ assets/js/firebaseModel.js
@@ -17,6 +17,26 @@ const getMovies = (uid) => {
     });
 };
 
+// tested: works without uid
+module.exports.getAllMovies = (uid) => {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            // dateWatched == null (isn't defined)
+            url: `https://titanium-logic.firebaseio.com/movies.json?orderBy="uid"&equalTo=${uid}`
+        }).done(movies => resolve(movies))
+        .fail(error => reject(error));
+    });
+};
+
+module.exports.getMovieByIdAndUid = (id,uid)=>{
+    return new Promise((resolve,reject)=>{
+        module.exports.getMoviesById(id)
+        .then((list)=>{
+            resolve(_.findKey(list,['uid',uid]));
+        });
+    });
+};
+
 module.exports.rateMovie = (uid, movieId, stars) => {
     // determine number of filled stars for movie
 };
@@ -37,11 +57,11 @@ module.exports.deleteMovie = (uid, movieId) => {
     
 };
 
-module.exports.searchMovies = (uid, term) => {
+module.exports.searchMovies = (term, uid) => {
     return new Promise((resolve, reject) => {
         getMovies(uid).then(movies => {
             let regex = new RegExp(term, "i");
-            resolve(movies.filter(movie => regex.test(movie.title)));
+            resolve(_.filter(movies, movie => regex.test(movie.title)));
         })
         .catch(error => reject(error));
     });
--- assets/js/view.js
+++ assets/js/view.js
@@ -8,17 +8,17 @@ const movieControls = require("./../templates/controls.hbs");
 const firebase = require("firebase");
 
 module.exports.checkLogin = () => {
-  // Listen to if the user is logged in
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) { // User is logged   in
-      $("#auth-btn").hide();
-      $("#logout-btn").show();
-    } else { // No user is logged in
-      $("#logout-btn").hide();
-      $("#auth-btn").show();
-    }
-  });
-};
+    // Listen to if the user is logged in
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) { // User is logged   in
+        $("#auth-btn").hide();
+        $("#logout-btn").show();
+      } else { // No user is logged in
+        $("#logout-btn").hide();
+        $("#auth-btn").show();
+      }
+    });
+  };
 
 
 module.exports.addDetails = (movie) => {
@@ -48,26 +48,47 @@ module.exports.addDetails = (movie) => {
 
   return movie;
 };
-module.exports.showMovies = (moviesArr)=>{
-  clearMovies();
 
+// Add  objects for handelbars to loop over to know the count.  Handlebars does not have a for loop, but it does have foreach.
 
-  moviesArr.map(movie => {
-    movie.release_date = movie.release_date.substring(0, 4);
+module.exports.showMovies = (moviesArr) => {
+    clearMovies();
 
-    movie = module.exports.addDetails(movie);
+    moviesArr.map(movie => {
+        movie.release_date = movie.release_date.substring(0, 4);
 
-    return movie;
-  });
+        if (typeof movie.stars !== "undefined") {
+            if (movie.stars >= 8) {
+                movie.class = "favorite";
+            } else if (movie.stars === 0) {
+                movie.class = "wishlist";
+            } else {
+                movie.class = "watched";
+            }            let movieStars = [];
+            for (let i = 0; i < 10; i++) {
+                if (i < movie.stars) {
+                    movieStars.push({ star: true });
+                } else {
+                    movieStars.push({});
+                }
+            }
+            movie.stars = movieStars;
 
-    moviesArr.forEach((movie, key) => {
-        if (key < 6) {
-            tmdb.getCast(movie.id)
-                .then(cast => {
-                    movie.cast = cast;
-                    $('#movieList > .row').append(movieCard({ movie }));
-                });
+        } else {
+            movie.class = "tmdb";
         }
+        return movie;
+    });
+
+    // adds movies in order of search, not promise fulfillment
+    let castPromises = moviesArr.map(movie => {
+        return tmdb.getCast(movie.id);
+    });
+    Promise.all(castPromises).then(casts => {
+        moviesArr.forEach((movie, index) => {
+            movie.cast = casts[index];
+            $('#movieList > .row').append(movieCard({ movie }));
+        });
     });
 };
 
