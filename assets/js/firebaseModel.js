8ae064eca2031695d88d7097523fb1abc4b0ff09
Hunter Phillips <hkphillips42@Hunters-MacBook-Pro.local>
2018-01-23 09:52:02 -0600

    rateMovie

--- assets/js/firebaseModel.js
+++ assets/js/firebaseModel.js
@@ -2,22 +2,44 @@
 
 const firebase = require("./fbConfig");
 const $ = require("jquery");
+const _ = require('lodash');
 
 const fbURL = "https://titanium-logic.firebaseio.com";
 
 // tested: works without uid
-module.exports.getMovies = (uid) => {
+module.exports.getMoviesById = (id) => {
     return new Promise((resolve, reject) => {
         $.ajax({
             // dateWatched == null (isn't defined)
-            url: `https://titanium-logic.firebaseio.com/movies.json?orderBy="dateAdded"`
+            url: `https://titanium-logic.firebaseio.com/movies.json?orderBy="id"&equalTo=${id}`
         }).done(wishlist => resolve(wishlist))
         .fail(error => reject(error));
     });
 };
 
+module.exports.getMovieByIdAndUid = (id,uid)=>{
+    return new Promise((resolve,reject)=>{
+        module.exports.getMoviesById(id)
+        .then((list)=>{
+            resolve(_.findKey(list,['uid',uid]));
+        });
+    });
+};
+
 module.exports.rateMovie = (uid, movieId, stars) => {
-    // determine number of filled stars for movie
+    let rating = {"stars":stars};
+    module.exports.getMovieByIdAndUid(movieId, uid)
+    .then((key)=>{
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `${fbURL}/movies/${key}.json`,
+                method: "PATCH",
+                data: JSON.stringify(rating)
+            }).done(() => {
+                resolve();
+            });
+        }); 
+    });
 };
 
 module.exports.addMovie = (newMovie) => {
