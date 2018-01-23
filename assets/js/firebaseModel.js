f17f9da94892ebd4ca64abb22f16f1694fa91913
Tim A. Conner <timaconner1@gmail.com>
2018-01-23 11:49:11 -0600

    Removed console logs.

--- assets/js/firebaseModel.js
+++ assets/js/firebaseModel.js
@@ -2,54 +2,31 @@
 
 const firebase = require("./fbConfig");
 const $ = require("jquery");
-const _ = require('lodash');
+const _ = require("lodash");
+
 const fbURL = "https://titanium-logic.firebaseio.com";
 
 // tested: works without uid
-module.exports.getMoviesById = (id) => {
+const getMovies = (uid) => {
     return new Promise((resolve, reject) => {
         $.ajax({
             // dateWatched == null (isn't defined)
-            url: `https://titanium-logic.firebaseio.com/movies.json?orderBy="id"&equalTo=${id}`
-        }).done(wishlist => resolve(wishlist))
+            url: `https://titanium-logic.firebaseio.com/movies.json?orderBy="dateAdded"`
+        }).done(movies => resolve(_.values(movies)))
         .fail(error => reject(error));
     });
 };
 
-module.exports.getMovieByIdAndUid = (id,uid)=>{
-    return new Promise((resolve,reject)=>{
-        module.exports.getMoviesById(id)
-        .then((list)=>{
-            resolve(_.findKey(list,['uid',uid]));
-        });
-    });
-};
-
 module.exports.rateMovie = (uid, movieId, stars) => {
-    let rating = {"stars":stars};
-    module.exports.getMovieByIdAndUid(movieId, uid)
-    .then((key)=>{
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: `${fbURL}/movies/${key}.json`,
-                method: "PATCH",
-                data: JSON.stringify(rating)
-            }).done(() => {
-                resolve();
-            });
-        }); 
-    });
+    // determine number of filled stars for movie
 };
 
 module.exports.addMovie = (newMovie) => {
-    let movieJson = JSON.stringify(newMovie);
-    console.log('movieJson', movieJson);
-    
     return new Promise((resolve, reject) => {
         $.ajax({
             url: `${fbURL}/movies.json`,
             method: "POST",
-            data: movieJson
+            data: JSON.stringify(newMovie)
         }).done(data => {
             resolve(data);
         });
@@ -62,10 +39,12 @@ module.exports.deleteMovie = (uid, movieId) => {
 
 module.exports.searchMovies = (uid, term) => {
     return new Promise((resolve, reject) => {
-        module.exports.getMoviesById(uid).then(movies => {
+        getMovies(uid).then(movies => {
             let regex = new RegExp(term, "i");
             resolve(movies.filter(movie => regex.test(movie.title)));
         })
         .catch(error => reject(error));
     });
 };
+
+module.exports.getMovies = getMovies;
