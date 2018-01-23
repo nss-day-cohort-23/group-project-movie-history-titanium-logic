a66c3d8a9cd5fdb1179c34b27f2fb891dd3a2ed6
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-01-23 11:51:10 -0600

    indented view, made showMovies go in order of search, not promise speed

--- assets/js/view.js
+++ assets/js/view.js
@@ -7,64 +7,63 @@ const movieCard = require("./../templates/movieCard.hbs");
 const firebase = require("firebase");
 
 module.exports.viewLogin = () => {
-  // Listen to if the user is logged in
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) { // User is logged   in
-      $("#auth-btn").hide();
-      $("#logout-btn").show();
-    } else { // No user is logged in
-      $("#logout-btn").hide();
-      $("#auth-btn").show();
-    }
-  });
+    // Listen to if the user is logged in
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) { // User is logged   in
+            $("#auth-btn").hide();
+            $("#logout-btn").show();
+        } else { // No user is logged in
+            $("#logout-btn").hide();
+            $("#auth-btn").show();
+        }
+    });
 };
 
 module.exports.showMovies = (moviesArr) => {
-  clearMovies();
+    clearMovies();
 
-  moviesArr.map(movie => {
-    movie.release_date = movie.release_date.substring(0, 4);
+    moviesArr.map(movie => {
+        movie.release_date = movie.release_date.substring(0, 4);
 
-    if (typeof movie.stars !== "undefined") {
-      if (movie.stars >= 8) {
-        movie.class = "favorite";
-      } else if (movie.stars === 0) {
-        movie.class = "wishlist";
-      } else {
-        movie.class = "watched";
-      }
+        if (typeof movie.stars !== "undefined") {
+            if (movie.stars >= 8) {
+                movie.class = "favorite";
+            } else if (movie.stars === 0) {
+                movie.class = "wishlist";
+            } else {
+                movie.class = "watched";
+            }
 
-      // Add  objects for handelbars to loop over to know the count.  Handlebars does not have a for loop, but it does have foreach.
+            // Add  objects for handelbars to loop over to know the count.  Handlebars does not have a for loop, but it does have foreach.
 
-      let movieStars = [];
-      for (let i = 0; i < 10; i++) {
-        if (i < movie.stars) {
-          movieStars.push({ star: true });
+            let movieStars = [];
+            for (let i = 0; i < 10; i++) {
+                if (i < movie.stars) {
+                    movieStars.push({ star: true });
+                } else {
+                    movieStars.push({});
+                }
+            }
+            movie.stars = movieStars;
+
         } else {
-          movieStars.push({});
+            movie.class = "tmdb";
         }
-      }
-      movie.stars = movieStars;
+        return movie;
+    });
 
-    } else {
-      movie.class = "tmdb";
-    }
-
-
-    return movie;
-  });
-
-  moviesArr.forEach((movie, key) => {
-    if (key < 6) {
-      tmdb.getCast(movie.id)
-        .then(cast => {
-          movie.cast = cast;
-          $('#movieList > .row').append(movieCard({ movie }));
+    // adds movies in order of search, not promise fulfillment
+    let castPromises = moviesArr.map(movie => {
+        return tmdb.getCast(movie.id);
+    });
+    Promise.all(castPromises).then(casts => {
+        moviesArr.forEach((movie, index) => {
+            movie.cast = casts[index];
+            $('#movieList > .row').append(movieCard({ movie }));
         });
-    }
-  });
+    });
 };
 
 const clearMovies = () => {
-  $('#movieList > .row').html('');
+    $('#movieList > .row').html('');
 };
